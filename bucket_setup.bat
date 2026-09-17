278934330af83b9b15119d59d7ee039273d37748
aro-1790 <odelladam@outlook.com>
2026-09-17 16:06:10 -0500

    This is going to need more testing

--- bucket_setup.bat
+++ bucket_setup.bat
@@ -2,5 +2,9 @@ if defined SCOOP (set "SCOOP_DIR=%SCOOP%") else (set "SCOOP_DIR=%USERPROFILE%\sc
 
 git clone --depth 1 --filter=blob:none --no-checkout https://github.com/aro-1790/hookshot-launchinst "%SCOOP_DIR%\buckets\hookshot-launchinst"
 cd /D "%SCOOP_DIR%\buckets\hookshot-launchinst"
+
+git config pull.rebase true
+git config pull.ff only
+
 git sparse-checkout set --no-cone bucket/*
-git checkout
+git checkout -B master origin/master
